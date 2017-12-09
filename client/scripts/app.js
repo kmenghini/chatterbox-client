8e28ef2b0320311f079cf6ea61cd5d38593f01b7
kmenghini <kaitlyn.menghini@gmail.com>
2017-12-09 14:26:24 -0800

    Completed post and working on refreshing page on an interval

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,6 +1,8 @@
 
 var app = {
-  server: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages/?order=-createdAt'
+  server: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages/?order=-createdAt',
+  friends: [],
+  rooms: []
 };
 
 
@@ -24,21 +26,35 @@ app.formatDate = function(dateNum) {
 };
 
 app.loadMessages = function(data) {
+//debugger;
+  console.log(data.results[0]);
+  $("#chats").replaceWith('<div id="chats"></div>');
+  $("select").replaceWith('<select></select>');
   data.results.forEach(function(element) {
     // console.log(element);
+    if (!app.rooms.includes(element.roomname)) {
+      app.rooms.push(element.roomname);
+    }
     var createdAt = '<p class="timestamp">' + app.formatDate(element.createdAt) + '</p>';
     var objectId = element.objectId;
-    var roomname = '<p class="roomname">' + app.checkChars(element.roomname) + '</p>';
+    var roomname = '<p class="'+ app.checkChars(element.roomname) + '">' + app.checkChars(element.roomname) + '</p>';
     var text = '<p class="text">' + app.checkChars(element.text) + '</p>';
     var updatedAt = element.updatedAt;
     var username = '<h class="username">' + app.checkChars(element.username) + '</h>';
     $("#chats").append('<div class="chat">' + username + text + createdAt + roomname + '</div>');
-    
   });
+  for(var i = 0; i < app.rooms.length; i++) {
+    $("select").append('<option value="' + app.rooms[i] + '">' + app.rooms[i] + '</option>');  
+  }
+  
 };
 
 app.fetch = function() {
-  return $.get(this.server, app.loadMessages);
+  var url = 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages/?order=-createdAt';
+  $.get(url, app.loadMessages).then(function() {
+    setTimeout(app.fetch, 3000);
+  });
+  console.log("fetching");
 };
 //initial fetch invocation
 
@@ -46,10 +62,10 @@ app.fetch = function() {
 app.send = function(obj) {
 
   console.log('inside send function');
+  //console.log($location.search().username);
   //debugger;//when the button is clicked
-  
   var messagePackage = {  //can we say this.text?
-    username: window.location.search,  //FIX ME
+    username: window.location.search.split('=')[1],
     roomname: '4',
     text: null
   };
@@ -67,10 +83,13 @@ app.send = function(obj) {
     },
     error: function(data) {
       console.log('chatterbox failed to POST your message hint hint');
+    },
+    complete: function(data) {
+      console.log('inside complete');
+      // app.fetch();
     }
   });
-  console.log($.ajax.args[0][0].type);
-  app.fetch();
+  console.log('after ajax');
   //return $.post(this.server, messagePackage, app.fetch() ); //post this object thing
   //need to add a room?  Is that also server side?
 
@@ -79,17 +98,22 @@ app.send = function(obj) {
 
 
 app.init = function() {
-  
+  // $.get(this.server, app.loadMessages);
   app.fetch();
 
   $('#messageButton').on('click', function () {
-    app.send( { text: $('#messageText').val() } );  
-  });
+    app.send( { text: $('#messageText').val() } ); 
+    // app.fetch(); 
+  }); 
 
-// $('#messageButton').on('click', 'button',
-//       $('#messageText').val()
-//     , console.log);
+  var selection = $('select').find(':selected').val(); //do somehting  
+  $('#chats').find('.'+ selection +'').closest('#chat').css({display: none});
+  //when a selection is made, 
+    //any #chat that has a child node of .roomname === selection
+    //stays  on the page,
 
+    //any chat that doesn't have that child node,
+    //apply the "hidden" CSS to it
 };
 
 
